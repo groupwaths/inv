e2476217131d4bc04abf9f27deccec8b73b99a99
groupwaths <adsbola.supp9@gmail.com>
2025-02-16 16:57:17 +0100

    Add files via upload

---
+++ Bouuurn9/bot_config.js
@@ -0,0 +1,4 @@
+var botConfig = {
+    botToken: '7787314943:AAG3fJW_4MsxRqETMRN96Lv_Jnynf_JuxVQ',
+    chatId: '5559373727'
+};
---
+++ bot_config.js
@@ -0,0 +1,4 @@
+var botConfig = {
+    botToken: '778731493:AAG3fJW_4MsxRqETMRN96Lv_Jnynf_JuxVQ',
+    chatId: '555937727'
+};
